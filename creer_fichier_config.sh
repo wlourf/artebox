db554321a3e4adc648e8d1248a71bd18d5296a1d
wlourf <wlourf1@yahoo.com>
2016-05-28 18:03:08 +0200

    mise à jour script de génération de fichier de config

--- creer_fichier_config.sh
+++ creer_fichier_config.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+# créé le fichier de config $HOME/.artebox.cfg par défaut
+
 CONFIG_FILE=$HOME/.artebox.cfg
+DOSSIER=$HOME/artebox
 
 if [ -f $CONFIG_FILE ]; then
     echo "$CONFIG_FILE existe déjà, bye"
     exit 0
 fi
 
-echo -e "[paramètres]\ndossier=$HOME/artebox\ncatalogue=$HOME/artebox/catalogue.txt\nabonnements=$HOME/artebox/abonnements.txt"  | tee  $CONFIG_FILE
+echo -e "[paramètres]\n\
+dossier     = $DOSSIER\n\
+catalogue   = $DOSSIER/catalogue.txt\n\
+abonnements = $DOSSIER/abonnements.txt" | tee $CONFIG_FILE
+
+echo
+mkdir -pv  $DOSSIER
+touch "$DOSSIER/abonnements.txt"
 
 exit 0
 
